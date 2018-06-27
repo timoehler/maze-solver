513f252f3ada95c536485cb3068ac88114e859db
Tim Oehler <tim.oehler@bestbuy.com>
2018-06-27 12:53:18 -0500

    support for LESS extension

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /(\.less$|\.css$)/,
+        use: ['style-loader', 'css-loader', 'less-loader']
       }
     ]
   },

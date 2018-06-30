224ee97ef0a2c86d43dbabdc3312f074c5edff7f
Tim Oehler <tim.oehler@bestbuy.com>
2018-06-30 12:03:42 -0500

    added different maze sizes

--- src/client/App.js
+++ src/client/App.js
@@ -53,6 +53,62 @@ export default class App extends Component {
       .then(solution => this.setState({ solutionModel: solution.model }));
   }
 
+  resizeModel = (length) => {
+    const newModel = [];
+    for (let i = 0; i <= length; i++) {
+      const newRow = [];
+      for (let j = 0; j <= length; j++) {
+        newRow.push('.');
+      }
+      newModel.push(newRow);
+    }
+    this.setState({ model: newModel });
+  }
+
+
+  usePreset = (preset) => {
+    let newModel = [];
+
+    switch (preset) {
+      case '1':
+        newModel = [
+          ['A', '.', '.'],
+          ['#', '.', '.'],
+          ['#', '.', 'B']
+        ];
+        break;
+      case '2':
+        newModel = [
+          ['A', '.', '.'],
+          ['#', '.', '.'],
+          ['#', '.', 'B']
+        ];
+        break;
+      case '3':
+        newModel = [
+          ['A', '.', '.'],
+          ['#', '.', '.'],
+          ['#', '.', 'B']
+        ];
+        break;
+      case '4':
+        newModel = [
+          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+          ['#', 'A', '.', '.', '.', '#', '.', '.', '.', '#'],
+          ['#', '.', '#', '.', '#', '#', '.', '#', '.', '#'],
+          ['#', '.', '#', '.', '#', '#', '.', '#', '.', '#'],
+          ['#', '.', '#', '.', '.', '.', '.', '#', 'B', '#'],
+          ['#', '.', '#', '.', '#', '#', '.', '#', '.', '#'],
+          ['#', '.', '.', '.', '.', '#', '.', '.', '.', '#'],
+          ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
+        ];
+        break;
+      default:
+    }
+
+    this.setState({ model: newModel });
+  }
+
   render() {
     const { model, solutionModel, editorState } = this.state;
 
@@ -61,6 +117,16 @@ export default class App extends Component {
         <h1>Maze Solver</h1>
         <h2>Tool Palette</h2>
         <Editor selectionChanged={this.onEditorChanged} />
+        <h2>Maze type</h2>
+        <button type="submit" onClick={() => this.resizeModel('8')}>Small</button>
+        <button type="submit" onClick={() => this.resizeModel('16')}>Medium</button>
+        <button type="submit" onClick={() => this.resizeModel('32')}>Large</button>
+        <button type="submit" onClick={() => this.resizeModel('64')}>Extra-Large</button>
+        <button type="submit" onClick={() => this.usePreset('1')}>preset 1</button>
+        <button type="submit" onClick={() => this.usePreset('2')}>preset 2</button>
+        <button type="submit" onClick={() => this.usePreset('3')}>preset 3</button>
+        <button type="submit" onClick={() => this.usePreset('4')}>preset 4</button>
+
         <h2>Create your maze:</h2>
         <Grid editorType={editorState} model={model} modelChanged={this.onModelChanged} />
         <button type="submit" onClick={() => this.solve()}>Solve!</button>
